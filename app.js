bcb056715cc7a8198b6ebc5032145184b1f007a5
unknown <wbhaese@gmail.com>
2021-06-30 19:48:50 -0300

    Third class code

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const app = {
     data () {
         return {
             name: 'Wbh',
-            lastName: 'NoneH123'
+            lastName: 'NoneH123',
+            product: {
+                title: 'Filme 01',
+                description: 'Filme lego 1',
+                image: './assets/images/lego1.png'
+            },
+            product2: {
+                title: 'Filme 02',
+                description: 'Filme lego 2',
+                image: './assets/images/lego2.png'
+            }
         }
     }    
 } 
