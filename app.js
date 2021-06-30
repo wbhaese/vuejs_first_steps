66cd2cae8622cf3887da6ff30e766e037b52fd3d
unknown <wbhaese@gmail.com>
2021-06-30 19:57:48 -0300

    fourth class code

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const app = {
             product: {
                 title: 'Filme 01',
                 description: 'Filme lego 1',
-                image: './assets/images/lego1.png'
+                image: './assets/images/lego1.png',
+                stars: 3,
             },
             product2: {
                 title: 'Filme 02',
                 description: 'Filme lego 2',
-                image: './assets/images/lego2.png'
+                image: './assets/images/lego2.png',
+                stars: 0,
             }
         }
     }    
